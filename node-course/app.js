b4589c262f2aadd45275fcabb00548b4ea83e691
Margo Drozd <drozdmargo@i.ua>
2016-12-14 16:08:34 +0200

    modules OS, FS and external modules

--- node-course/app.js
+++ node-course/app.js
@@ -1,1 +1,13 @@
 console.log('starting app');
+
+const fs = require('fs');
+const os = require('os');
+const notes = require('./notes.js');
+
+var res = notes.addNote();
+console.log('Result:', notes.add(2, -7));
+
+var user = os.userInfo();
+
+
+fs.appendFile('greetings.txt', 'Hello'+ notes.age + ' ' + `hello ${user.username}`);
