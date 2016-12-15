d3d643890e09b7b721156247ce9dee08d805d869
Margo Drozd <drozdmargo@i.ua>
2016-12-15 14:05:24 +0200

    using lodash and method .uniq& .isString

--- node-course/app.js
+++ node-course/app.js
@@ -1,11 +1,18 @@
 const fs = require('fs');
 const os = require('os');
+const _ = require('lodash');
+
 const notes = require('./notes.js');
 
-var res = notes.addNote();
-console.log('Result:', notes.add(2, -7));
 
-var user = os.userInfo();
+console.log(_.isString(true));
+console.log(_.isString('grty'));
+var filterArray = _.uniq(['as', 1, 'as', 2, 3, 4]);
+console.log(filterArray);
+// var res = notes.addNote();
+// console.log('Result:', notes.add(2, -7));
+
+// var user = os.userInfo();
 
 
-fs.appendFile('greetings.txt', 'Hello'+ notes.age + ' ' + `hello ${user.username}`);
+// fs.appendFile('greetings.txt', 'Hello'+ notes.age + ' ' + `hello ${user.username}`);
