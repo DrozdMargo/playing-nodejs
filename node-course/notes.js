2ed1f1210da2a809972955001bce474bb1151885
Margo Drozd <drozdmargo@i.ua>
2016-12-16 20:38:45 +0200

    refactoring addNote function

--- node-course/notes.js
+++ node-course/notes.js
@@ -1,25 +1,31 @@
 console.log('starting notes.js');
 const fs = require('fs');
 
+var fetchNotes = () => {
+	try {
+		var notesString = fs.readFileSync('notes-data.json');
+		return  JSON.parse(notesString);
+	} catch (e) {
+		return []; 
+	}
+};
+
+var saveNotes = (notes) => {
+	fs.writeFileSync('notes-data.json', JSON.stringify(notes));  //it writes our new note into notes
+};
+
 var addNote = (title, body) => {
-	var notes = [];
+	var notes = fetchNotes();
 	var note = {
 		title,
 		body
 	};
-
-	try {
-		var notesString = fs.readFileSync('notes-data.json');
-		notes = JSON.parse(notesString);
-	} catch (e) {
-
-	}
-	
 	var duplicateNotes = notes.filter((note) => note.title === title);
 
 	if( duplicateNotes.length === 0 ) { // checked if exist title or not, if not we write this note
 		notes.push(note);
-		fs.writeFileSync('notes-data.json', JSON.stringify(notes));  //it writes our new note into notes
+		saveNotes(notes);
+		return note;
 	}	
 };
 
