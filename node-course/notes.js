df0c6802e9fd3a3aca015f9cf9c2d1ea026755f2
Margo Drozd <drozdmargo@i.ua>
2016-12-16 15:15:27 +0200

    write function for addNote

--- node-course/notes.js
+++ node-course/notes.js
@@ -1,12 +1,42 @@
 console.log('starting notes.js');
+const fs = require('fs');
 
+var addNote = (title, body) => {
+	var notes = [];
+	var note = {
+		title,
+		body
+	};
 
-module.exports.age = 25;
-module.exports.addNote = () => {
-	console.log('addNote');
-	return 'New note';
+	try {
+		var notesString = fs.readFileSync('notes-data.json');
+		notes = JSON.parse(notesString);
+	} catch (e) {
+
+	}
+	
+	var duplicateNotes = notes.filter((note) => note.title === title);
+
+	if( duplicateNotes.length === 0 ) { // checked if exist title or not, if not we write this note
+		notes.push(note);
+		fs.writeFileSync('notes-data.json', JSON.stringify(notes));  //it writes our new note into notes
+	}	
 };
 
-module.exports.add = (a, b ) => {
-	return a+b;
+var getAll = () => {
+	console.log('Getting all  notes');
+};
+
+var readNote = (title) => {
+	console.log('Reading note', title);
+};
+var removeNote = (title) => {
+	console.log('removing note', title);
+}
+
+module.exports = {
+	addNote,
+	getAll, // its equal addNote: addNote  (ES6)
+	readNote,
+	removeNote
 }
